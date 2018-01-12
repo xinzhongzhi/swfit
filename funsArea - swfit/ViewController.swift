78a5152182b212c3ab43c67b89c80f4f664c91ab
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2018-01-12 09:18:43 +0800

    Initial Commit

---
+++ funsArea - swfit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  funsArea - swfit
+//
+//  Created by 辛忠志 on 2018/1/12.
+//  Copyright © 2018年 xinzhongzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
